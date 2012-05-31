57c4ac98a38f21d19934a3ab953ded0c2a326ffb
Felipe Erices <fierices@uc.cl>
2012-05-31 15:33:02 -0400

    Further work on HBPLUS integration

--- src/pdidbanalyzer/HBPlus.java
+++ src/pdidbanalyzer/HBPlus.java
@@ -4,11 +4,14 @@
  */
 package pdidbanalyzer;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import com.sun.xml.internal.stream.buffer.stax.StreamReaderBufferCreator;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,8 +19,10 @@ import java.util.Arrays;
  */
 public class HBPlus {
 
-    private String program_out = "output.out";
-    private String program_error = "error.out";
+    private String programOut = "";
+    private String programError = "";
+    
+    public static int count = 1;
 
     /**
      * Runs the external program HBPlus
@@ -27,43 +32,110 @@ public class HBPlus {
     public void run(File file) {
 
         String[] args = new String[2];
-        args[0] = "../hbplus";
-        //args[1]="-X";
+        args[0] = "./hbplus";
+        //args[1]="-P";
         args[1] = file.getAbsolutePath();
 
         try {
 
+            
             ProcessBuilder pb = new ProcessBuilder(args);
-
-            File f = new File("Data_HBPlus");
-            f.mkdir();
-            pb.directory(f);
-
             Process p = pb.start();
             p.waitFor();
-
+            
             BufferedReader stdout = new BufferedReader(new InputStreamReader(p.getInputStream()));
             BufferedReader stderr = new BufferedReader(new InputStreamReader(p.getErrorStream()));
             String line;
-            System.out.println("Exit code: " + p.exitValue());
-            System.out.printf("Output of running %s is:", Arrays.toString(args));
+            
             while ((line = stdout.readLine()) != null) {
-                System.out.println(line);
+                programOut+=line+"\n";                
             }
-            System.out.println("");
-            System.out.printf("Stderr of running %s is:", Arrays.toString(args));
+            
             while ((line = stderr.readLine()) != null) {
-                System.out.println(line);
+                programError+=line+"\n";                
             }
-            System.out.println("");
-
+            
+            //<editor-fold defaultstate="collapsed" desc="Saving Output and Error in file -> Out_HBPlus.out">
+            FileWriter fw = new FileWriter("Out_HBPlus.out", true); 
+            fw.write("-----------------"+"HBPLus Output nº"+count+"-----------------"+"\n");
+            count ++;
+            fw.write("Exit code: " + p.exitValue()+"\n");
+            fw.write("Output of running "+Arrays.toString(args)+" is:"+"\n");
+            fw.write(line+"\n");
+            fw.write(""+"\n");
+            fw.write("Stderr of running "+ Arrays.toString(args)+" is:"+"\n");
+            fw.write(line+"\n");
+            fw.write("\n");
+            fw.write("---------------------------------------------------------\n");
+            fw.close();
+            //</editor-fold>
 
         } catch (InterruptedException ex) {
             //log.error(ex.toString(), ex);
         } catch (IOException ex) {
-            System.out.print(ex.toString() + "\n");
+            System.out.print(ex.toString() + "\n");            
         }
 
-
+    }
+    
+    private HBPlusRecord parseLine(String line) {
+        // Parse line
+        HBPlusRecord hbprecord = new HBPlusRecord();
+        hbprecord.setDonorResname(line.substring(6,9).trim());
+        hbprecord.setDonorResId(line.substring(1, 5).replaceFirst("^0+(?!$)", ""));
+        hbprecord.setDonorChainId(line.substring(0,1));
+        hbprecord.setDonorAtomname(line.substring(9,13).trim());
+        hbprecord.setDonorElement(hbprecord.getDonorAtomname().substring(0, 1));
+        
+        hbprecord.setAcceptorResname(line.substring(20,23).trim());
+        hbprecord.setAcceptorResId(line.substring(15, 19).replaceFirst("^0+(?!$)", ""));
+        hbprecord.setAcceptorChainId(line.substring(14,15));
+        hbprecord.setAcceptorAtomname(line.substring(23,27).trim());
+        hbprecord.setAcceptorElement(hbprecord.getAcceptorAtomname().substring(0, 1));
+        
+        
+        try
+        {
+            
+        
+            hbprecord.setDistDA(Float.parseFloat(line.substring(27, 32))) ;
+            hbprecord.setDistHA(Float.parseFloat(line.substring(52, 57)));
+            
+            hbprecord.setAngle_D_H_A(Float.parseFloat(line.substring(46, 51)));
+            hbprecord.setAngle_H_A_AA(Float.parseFloat(line.substring(58, 63)));
+            hbprecord.setAngle_D_A_AA(Float.parseFloat(line.substring(64, 69)));
+        }
+        catch (NumberFormatException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        // Fill HBPlusRecord object
+        return hbprecord;
+    }
+    
+    public List<HBPlusRecord> parse(String filename) {
+        
+        
+        List<HBPlusRecord> hb2Contents = new ArrayList<HBPlusRecord>();
+        String newfilename = filename.substring(0,filename.lastIndexOf("."))+".hb2";        
+        
+        try {            
+            
+            FileInputStream fstream = new FileInputStream(newfilename);
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if(Character.isDigit(line.charAt(1))&&Character.isDigit(line.charAt(2)))
+                {
+                    hb2Contents.add(parseLine(line));
+                }
+            }
+            in.close();
+            
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        }
+        return hb2Contents;
     }
 }
--- src/pdidbanalyzer/HBPlusRecord.java
+++ src/pdidbanalyzer/HBPlusRecord.java
@@ -4,24 +4,193 @@
  */
 package pdidbanalyzer;
 
+import java.security.KeyStore;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.interfaces.IAtom;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.interfaces.IPDBAtom;
+
 /**
  *
  * @author pipe
  */
-public class HBPlusRecord {
+public class HBPlusRecord { 
+    private String donorResname;
+    private String donorResId;
+    private String donorChainId;
+    private String donorAtomname;
+    private String donorElement;
     
-    private String donor_Chain_id;
-    private String donor_resname;
-    private String donor_atomname;
+    private String acceptorResname;
+    private String acceptorResId;
+    private String acceptorChainId;
+    private String acceptorAtomname;
+    private String acceptorElement;  
     
-    private String acceptor_Chain_id;
-    private String acceptor_resname;
-    private String acceptor_atomname;
+    private Float DistDA;
+    private Float DistHA;
     
-    private Float Dist_DA;
+    private Float Angle_D_H_A;
+    private Float Angle_D_A_AA;
+    private Float Angle_H_A_AA;
     
     
+    //<editor-fold defaultstate="collapsed" desc="Geters">
+
+    public Float getAngle_D_A_AA() {
+        return Angle_D_A_AA;
+    }
+
+    public void setAngle_D_A_AA(Float Angle_D_A_AA) {
+        this.Angle_D_A_AA = Angle_D_A_AA;
+    }
+
+    public Float getAngle_D_H_A() {
+        return Angle_D_H_A;
+    }
+
+    public void setAngle_D_H_A(Float Angle_D_H_A) {
+        this.Angle_D_H_A = Angle_D_H_A;
+    }
+
+    public Float getAngle_H_A_AA() {
+        return Angle_H_A_AA;
+    }
+
+    public void setAngle_H_A_AA(Float Angle_H_A_AA) {
+        this.Angle_H_A_AA = Angle_H_A_AA;
+    }
+
+    public Float getDistDA() {
+        return DistDA;
+    }
+
+    public void setDistDA(Float DistDA) {
+        this.DistDA = DistDA;
+    }
+
+    public Float getDistHA() {
+        return DistHA;
+    }
+
+    public void setDistHA(Float DistHA) {
+        this.DistHA = DistHA;
+    }
+
+    public String getAcceptorChainId() {
+        return acceptorChainId;
+    }
+
+    public void setAcceptorChainId(String acceptoChainId) {
+        this.acceptorChainId = acceptoChainId;
+    }
+
+    public String getAcceptorAtomname() {
+        return acceptorAtomname;
+    }
+
+    public void setAcceptorAtomname(String acceptorAtomname) {
+        this.acceptorAtomname = acceptorAtomname;
+    }
+
+    public String getAcceptorElement() {
+        return acceptorElement;
+    }
+
+    public void setAcceptorElement(String acceptorElement) {
+        this.acceptorElement = acceptorElement;
+    }
+
+    public String getAcceptorResId() {
+        return acceptorResId;
+    }
+
+    public void setAcceptorResId(String acceptorResId) {
+        this.acceptorResId = acceptorResId;
+    }
+
+    public String getAcceptorResname() {
+        return acceptorResname;
+    }
+
+    public void setAcceptorResname(String acceptorResname) {
+        this.acceptorResname = acceptorResname;
+    }
+
+    public String getDonorAtomname() {
+        return donorAtomname;
+    }
+
+    public void setDonorAtomname(String donorAtomname) {
+        this.donorAtomname = donorAtomname;
+    }
+
+    public String getDonorChainId() {
+        return donorChainId;
+    }
+
+    public void setDonorChainId(String donorChainId) {
+        this.donorChainId = donorChainId;
+    }
+
+    public String getDonorElement() {
+        return donorElement;
+    }
+
+    public void setDonorElement(String donorElement) {
+        this.donorElement = donorElement;
+    }
+
+    public String getDonorResId() {
+        return donorResId;
+    }
+
+    public void setDonorResId(String donorResId) {
+        this.donorResId = donorResId;
+    }
+
+    public String getDonorResname() {
+        return donorResname;
+    }
+
+    public void setDonorResname(String donorResname) {
+        this.donorResname = donorResname;
+    }
     
+
+    
+    //</editor-fold>
+        
+    public HBPlusRecord(){
+        
+    }
+    
+    public IPDBAtom GetAcceptor(){
+        
+        IChemObjectBuilder builder = DefaultChemObjectBuilder.getInstance();
+        
+        IPDBAtom pdbatom = builder.newPDBAtom(acceptorElement);        
+        pdbatom.setChainID(acceptorChainId);
+        pdbatom.setName(acceptorAtomname);
+        pdbatom.setResName(acceptorResname);
+        pdbatom.setResSeq(acceptorResId);
+        
+        IAtom iatom = builder.newAtom(acceptorElement);
+        
+        
+        return pdbatom;
+    }
+    
+    public IPDBAtom GetDonor(){
+        
+        IChemObjectBuilder builder = DefaultChemObjectBuilder.getInstance();
+        IPDBAtom pdbatom = builder.newPDBAtom(donorElement);
+        pdbatom.setChainID(donorChainId);
+        pdbatom.setName(donorAtomname);
+        pdbatom.setResName(donorResname);
+        pdbatom.setResSeq(donorResId);
+        return pdbatom;
+    }
     
     
     
