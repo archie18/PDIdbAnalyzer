a5d1f2bd07160ed76b8f5ffee73529d323222c25
Andreas Schueller <apschueller@gmail.com>
2012-11-16 14:41:27 -0300

    Added some test cases for new functionality (eff/noeff, mode2/4)

--- test/pdidbanalyzer/PDIdbAnalyzerTest.java
+++ test/pdidbanalyzer/PDIdbAnalyzerTest.java
@@ -56,19 +56,123 @@ public class PDIdbAnalyzerTest {
     /**
      * Test of main method, of class PDIdbAnalyzer.
      */
+//    @Test
+//    public void testMain() throws FileNotFoundException, IOException {
+//        System.out.println("main");
+//        String[] args = new String[2];
+//        args[0] = "-f testdata/1bl0_1_1.ent.gz";
+//        args[1] = "-m 2";
+//        PDIdbAnalyzer.main(args);
+//        List<String> expResult = readFile("testdata/1bl0_1_1.dat");
+//        List<String> result = readFile("1bl0_1_1.dat");
+//        for (int i = 1; i < result.size(); i++) { // Ignore first line which contains a timestamp
+//            assertEquals("Line " + (i+1), expResult.get(i), result.get(i));
+//        }
+//    }
+
+    /**
+     * Test effective interactions, w/o HBPLUS, 7.0, fouratoms.pdb
+     */
     @Test
-    public void testMain() throws FileNotFoundException, IOException {
-        System.out.println("main");
+    public void testMainEffNoHbplus7Fouratoms() throws FileNotFoundException, IOException {
+        System.out.println("testMainEffNoHbplus7Fouratoms");
         String[] args = new String[2];
-        args[0] = "-f testdata/1bl0_1_1.ent.gz";
+        args[0] = "-f testdata/fouratoms.pdb";
         args[1] = "-m 2";
         PDIdbAnalyzer.main(args);
-        List<String> expResult = readFile("testdata/1bl0_1_1.dat");
-        List<String> result = readFile("1bl0_1_1.dat");
-        for (int i = 0; i < result.size(); i++) {
+        List<String> expResult = readFile("testdata/fouratoms_EffNoHbplus7.dat");
+        List<String> result = readFile("fouratoms.dat");
+        for (int i = 1; i < result.size(); i++) { // Ignore first line which contains a timestamp 
             assertEquals("Line " + (i+1), expResult.get(i), result.get(i));
         }
-        
+    }
+
+    /**
+     * Test all interactions, w/o HBPLUS, 7.0, fouratoms.pdb
+     */
+    @Test
+    public void testMainNoEffNoHbplus7Fouratoms() throws FileNotFoundException, IOException {
+        System.out.println("testMainNoEffNoHbplus7Fouratoms");
+        String[] args = new String[3];
+        args[0] = "-f testdata/fouratoms.pdb";
+        args[1] = "-m 2";
+        args[2] = "--noeff";
+        PDIdbAnalyzer.main(args);
+        List<String> expResult = readFile("testdata/fouratoms_NoEffNoHbplus7.dat");
+        List<String> result = readFile("fouratoms.dat");
+        for (int i = 1; i < result.size(); i++) { // Ignore first line which contains a timestamp 
+            assertEquals("Line " + (i+1), expResult.get(i), result.get(i));
+        }
+    }
+
+    /**
+     * Test effective interactions, w/o HBPLUS, 7.0, sixatoms.pdb
+     */
+    @Test
+    public void testMainEffNoHbplus7Sixatoms() throws FileNotFoundException, IOException {
+        System.out.println("testMainEffNoHbplus7Sixatoms");
+        String[] args = new String[2];
+        args[0] = "-f testdata/sixatoms.pdb";
+        args[1] = "-m 2";
+        PDIdbAnalyzer.main(args);
+        List<String> expResult = readFile("testdata/sixatoms_EffNoHbplus7.dat");
+        List<String> result = readFile("sixatoms.dat");
+        for (int i = 1; i < result.size(); i++) { // Ignore first line which contains a timestamp 
+            assertEquals("Line " + (i+1), expResult.get(i), result.get(i));
+        }
+    }
+
+    /**
+     * Test all interactions, w/o HBPLUS, 7.0, sixatoms.pdb
+     */
+    @Test
+    public void testMainNoEffNoHbplus7Sixatoms() throws FileNotFoundException, IOException {
+        System.out.println("testMainNoEffNoHbplus7Sixatoms");
+        String[] args = new String[3];
+        args[0] = "-f testdata/sixatoms.pdb";
+        args[1] = "-m 2";
+        args[2] = "--noeff";
+        PDIdbAnalyzer.main(args);
+        List<String> expResult = readFile("testdata/sixatoms_NoEffNoHbplus7.dat");
+        List<String> result = readFile("sixatoms.dat");
+        for (int i = 1; i < result.size(); i++) { // Ignore first line which contains a timestamp 
+            assertEquals("Line " + (i+1), expResult.get(i), result.get(i));
+        }
+    }
+
+    /**
+     * Test effective interactions, w/o HBPLUS, 7.0, sixatoms_pantano.pdb
+     */
+    @Test
+    public void testMainEffNoHbplus7PantanoSixatoms() throws FileNotFoundException, IOException {
+        System.out.println("testMainEffNoHbplus7PantanoSixatoms");
+        String[] args = new String[2];
+        args[0] = "-f testdata/sixatoms_pantano.pdb";
+        args[1] = "-m 4";
+        PDIdbAnalyzer.main(args);
+        List<String> expResult = readFile("testdata/sixatoms_pantano_EffNoHbplus7.dat");
+        List<String> result = readFile("sixatoms_pantano.dat");
+        for (int i = 1; i < result.size(); i++) { // Ignore first line which contains a timestamp 
+            assertEquals("Line " + (i+1), expResult.get(i), result.get(i));
+        }
+    }
+
+    /**
+     * Test effective interactions, w/o HBPLUS, 7.0, sixatoms_pantano.pdb
+     */
+    @Test
+    public void testMainNoEffNoHbplus7PantanoSixatoms() throws FileNotFoundException, IOException {
+        System.out.println("testMainNoEffNoHbplus7PantanoSixatoms");
+        String[] args = new String[3];
+        args[0] = "-f testdata/sixatoms_pantano.pdb";
+        args[1] = "-m 4";
+        args[2] = "--noeff";
+        PDIdbAnalyzer.main(args);
+        List<String> expResult = readFile("testdata/sixatoms_pantano_NoEffNoHbplus7.dat");
+        List<String> result = readFile("sixatoms_pantano.dat");
+        for (int i = 1; i < result.size(); i++) { // Ignore first line which contains a timestamp 
+            assertEquals("Line " + (i+1), expResult.get(i), result.get(i));
+        }
     }
 
 }
